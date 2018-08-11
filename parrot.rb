fa1b739ea07ca06161d7b8b5ff04972e879ba2f4
Rob Parker <congo.quinine@gmail.com>
2018-08-11 19:16:42 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(Squawk='Hlee')
-  puts Squawk
-  return Squawk
+def parrot(boo="Squawk!")
+  puts boo
+  return boo
 end
 
 parrot()
